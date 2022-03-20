432c2539c92486231fedc0404b5a49eafc5755b3
dankito <git@dankito.net>
2022-03-20 20:35:07 +0100

    Added WebView displaying Stereostrand home page

---
+++ Stereostrand/view/WebView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+import WebKit
+
+
+struct WebView: UIViewRepresentable {
+    
+    let url = "https://www.stereostrand.de/"
+    
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        
+        webView.load(URLRequest(url: URL(string: self.url)!))
+        
+        return webView
+    }
+ 
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        
+    }
+    
+    
+    class Coordinator: NSObject, WKNavigationDelegate {
+        
+        var parent: WebView
+        
+        init(_ uiWebView: WebView) {
+            self.parent = uiWebView
+        }
+        
+    }
+
+}
+
+
+struct WebView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        WebView()
+    }
+
+}
